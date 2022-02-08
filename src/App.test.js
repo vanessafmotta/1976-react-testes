f5f23ceafcf68fb22274aca66d33af0a5fd6a68e
Vanessa Motta <mottavns@gmail.com>
2022-02-07 23:34:55 -0300

    teste-função

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render, screen } from "@testing-library/react"
-import App from './App';
+import App, { calcularNovoSaldo } from './App';
 
 describe('Componente principal', () => {
+	//teste de um componente
 	describe('Quando eu abro o app do banco', () => {
 		it('o nome é exibido', () => {
 			render(<App />);
@@ -18,6 +19,19 @@ describe('Componente principal', () => {
 			render(<App />);
 
 			expect(screen.getByText('Realizar operação')).toBeInTheDocument();
+		})
+	})
+	// teste de uma funçao
+	describe('Quando eu realizo uma transação', () =>{
+		it('que é um saque, o valor vai diminuir', () =>{
+			const valores = {
+				transacao: 'saque',
+				valor: 50
+			}
+			// o que eu tenho
+			const novoSaldo = calcularNovoSaldo(valores, 150)
+			//o que eu sei que vai ser a reposta 
+			expect(novoSaldo).toBe(100);
 		})
 	})
 })
